b8b7491c34055adff3d76b10bb57603848b4943b
Matthew C <Chapman.L.Matthew@gmail.com>
2021-11-09 11:53:23 -0800

    Launch calculator with skeleton Skill classes, empty unique panels are better than duplicate mirror panels

--- src/main/java/nop/matthew/osrscalculator/ui/OSRSCalculator.java
+++ src/main/java/nop/matthew/osrscalculator/ui/OSRSCalculator.java
@@ -1,7 +1,16 @@
 package nop.matthew.osrscalculator.ui;
 
 import nop.matthew.osrscalculator.data.construction.Construction;
+import nop.matthew.osrscalculator.data.cooking.Cooking;
+import nop.matthew.osrscalculator.data.crafting.Crafting;
+import nop.matthew.osrscalculator.data.farming.Farming;
+import nop.matthew.osrscalculator.data.firemaking.Firemaking;
+import nop.matthew.osrscalculator.data.fletching.Fletching;
 import nop.matthew.osrscalculator.data.herblore.Herblore;
+import nop.matthew.osrscalculator.data.hunter.Hunter;
+import nop.matthew.osrscalculator.data.magic.Magic;
+import nop.matthew.osrscalculator.data.prayer.Prayer;
+import nop.matthew.osrscalculator.data.smithing.Smithing;
 import nop.matthew.osrscalculator.net.PriceFetcher;
 
 import java.io.IOException;
@@ -15,31 +24,44 @@ public class OSRSCalculator {
 	public static void main(String[] args) throws IOException {
 		PriceFetcher priceFetcher = new PriceFetcher();
 
-		Herblore herblore = new Herblore(priceFetcher);
 		Construction construction = new Construction(priceFetcher);
+		Cooking cooking = new Cooking(priceFetcher);
+		Crafting crafting = new Crafting(priceFetcher);
+		Farming farming = new Farming(priceFetcher);
+		Firemaking firemaking = new Firemaking(priceFetcher);
+		Fletching fletching = new Fletching(priceFetcher);
+		Herblore herblore = new Herblore(priceFetcher);
+		Hunter hunter = new Hunter(priceFetcher);
+		Magic magic = new Magic(priceFetcher);
+		Prayer prayer = new Prayer(priceFetcher);
+		Smithing smithing = new Smithing(priceFetcher);
 
-
-		herblore.addRecipePrices();
 		construction.addRecipePrices();
+		cooking.addRecipePrices();
+		crafting.addRecipePrices();
+		farming.addRecipePrices();
+		firemaking.addRecipePrices();
+		fletching.addRecipePrices();
+		herblore.addRecipePrices();
+		hunter.addRecipePrices();
+		magic.addRecipePrices();
+		prayer.addRecipePrices();
+		smithing.addRecipePrices();
 
 		priceFetcher.updatePrices();
 
 		Calculator calculator = new Calculator();
-		/*
-		Until all skills are implemented, add an
-		equal amount of buttons using duplicate skills
-		 */
 		calculator.addSkill(construction);
+		calculator.addSkill(cooking);
+		calculator.addSkill(crafting);
+		calculator.addSkill(farming);
+		calculator.addSkill(firemaking);
+		calculator.addSkill(fletching);
 		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
-		calculator.addSkill(herblore);
+		calculator.addSkill(hunter);
+		calculator.addSkill(magic);
+		calculator.addSkill(prayer);
+		calculator.addSkill(smithing);
 
 		calculator.openSwing(DEFAULT_APP_WIDTH, DEFAULT_APP_HEIGHT);
 	}
