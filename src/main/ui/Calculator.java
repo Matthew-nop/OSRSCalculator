dbc8e6e3a3b9fb25d711d1a22e8dd99ec3456a5d
Matthew C <Chapman.L.Matthew@gmail.com>
2021-11-08 18:49:38 -0800

    Implement WIP UI, proof of concept

---
+++ src/main/ui/Calculator.java
@@ -0,0 +1,80 @@
+package ui;
+
+import data.Skill;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+public class Calculator {
+	public static final int SKILL_ICON_LENGTH = 40;
+	public static final int METHOD_SELECTION_WIDTH = 160;
+	public static final int METHOD_SELECTION_HEIGHT = 40;
+	public static final int MINIMUM_WIDTH = OSRSCalculator.SKILL_COUNT*SKILL_ICON_LENGTH;
+	public static final int MINIMUM_HEIGHT = SKILL_ICON_LENGTH + METHOD_SELECTION_HEIGHT;
+
+	private final JFrame mainFrame;
+	private final BorderLayout mainLayout;
+	private final SelectionPanel selectionPanel;
+	private final JPanel resultPanel;
+
+	public Calculator() {
+		this.mainFrame = new JFrame("OSRS Calculator");
+		this.mainLayout = new BorderLayout(0, 0);
+		this.mainFrame.setUndecorated(true);
+		this.mainFrame.setMinimumSize(new Dimension(MINIMUM_WIDTH, MINIMUM_HEIGHT));
+		this.selectionPanel = new SelectionPanel();
+		this.resultPanel = new JPanel(new CardLayout());
+	}
+
+	public void openSwing(int width, int height) {
+		// Set up the main frame
+		setup();
+		this.mainFrame.setPreferredSize(new Dimension(width, height));
+
+		// Set up the selection panel
+		this.mainFrame.add(BorderLayout.NORTH, this.selectionPanel);
+		this.selectionPanel.setup();
+
+		// Set up the skills panel
+		this.mainFrame.add(BorderLayout.CENTER, resultPanel);
+		this.resultPanel.setVisible(true);
+
+		// Finish
+		this.mainFrame.setVisible(true);
+	}
+
+	private void setup() {
+		this.mainFrame.setLayout(this.mainLayout);
+		this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.mainFrame.setResizable(true);
+	}
+
+	public void addSkill(Skill skill) throws IOException {
+		String key = skill.getKey();
+
+		SkillPanel panel = new SkillPanel(skill);
+		this.resultPanel.add(key, panel.getScrollPane());
+
+		JButton button = new JButton(new ImageIcon(ImageIO
+				.read(new File("res/SkillIcons/" + key.toLowerCase() + ".png"))
+				.getScaledInstance(SKILL_ICON_LENGTH, SKILL_ICON_LENGTH, Image.SCALE_DEFAULT)));
+		button.setToolTipText(key);
+		button.addActionListener(e -> {
+			System.out.println("Selected calculator panel for " + key);
+			panel.update();
+			this.selectionPanel.setMethods(skill.getMethodRecipes().keySet().toArray(new String[]{}));
+			CardLayout layout = (CardLayout) (resultPanel.getLayout());
+			layout.show(resultPanel, key);
+		});
+		this.selectionPanel.addButton(button);
+	}
+}
---
+++ src/main/ui/SelectionPanel.java
@@ -0,0 +1,46 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SelectionPanel extends JPanel {
+	private final JPanel buttons;
+	private final JComboBox<String> selectedMethod;
+
+	public SelectionPanel() {
+		super();
+		setLayout(new BorderLayout(0, 0));
+		this.buttons = new JPanel(new GridLayout(1, 11));
+		this.selectedMethod = new JComboBox<>();
+	}
+
+	public void setup() {
+		Dimension buttonsSize = new Dimension(Calculator.MINIMUM_WIDTH, Calculator.SKILL_ICON_LENGTH);
+		this.buttons.setMinimumSize(buttonsSize);
+		add(BorderLayout.NORTH, buttons);
+
+		this.selectedMethod.setMinimumSize(new Dimension(Calculator.METHOD_SELECTION_WIDTH, Calculator.METHOD_SELECTION_HEIGHT));
+		add(BorderLayout.EAST, selectedMethod);
+
+		setLocation(0, 0);
+		Dimension size = new Dimension(Calculator.MINIMUM_WIDTH, Calculator.SKILL_ICON_LENGTH + Calculator.METHOD_SELECTION_HEIGHT);
+		setMinimumSize(size);
+		setPreferredSize(size);
+		setVisible(true);
+	}
+
+	public void addButton(JButton button) {
+		Dimension size = new Dimension(Calculator.SKILL_ICON_LENGTH, Calculator.SKILL_ICON_LENGTH);
+		button.setMinimumSize(size);
+		button.setMaximumSize(size);
+		button.setPreferredSize(size);
+		buttons.add(button);
+	}
+
+	public void setMethods(String[] methods) {
+		this.selectedMethod.removeAllItems();
+		for (String method : methods) {
+			this.selectedMethod.addItem(method);
+		}
+	}
+}
