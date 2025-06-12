4abe0134fc89d9d2876a1523395a99223a6b66df
Matthew C <Matthew.nop.dev@gmail.com>
2025-06-12 17:23:00 -0400

    UI: Prevent flags window from opening if no flags apply to skill

--- src/main/java/nop/matthew/osrscalculator/ui/FlagsWindow.java
+++ src/main/java/nop/matthew/osrscalculator/ui/FlagsWindow.java
@@ -21,7 +21,6 @@ public class FlagsWindow extends JDialog {
 		setLayout(layoutManager);
 		setModal(true);
 
-
 		Flags[] flags = Flags.values();
 		int count = 0;
 		for (Flags flag : flags) {
@@ -38,6 +37,12 @@ public class FlagsWindow extends JDialog {
 				count++;
 			}
 		}
+
+		if (count == 0) {
+			dispose();
+			return;
+		}
+
 		layoutManager.setRows(2);
 		layoutManager.setColumns(count / 2 + 1);
 
