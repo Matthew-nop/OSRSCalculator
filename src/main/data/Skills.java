d500de5f39d67b6c6274a798c02c5a4743d38382
Matthew C <Matthew.nop.dev@gmail.com>
2025-06-12 17:22:48 -0400

    Add skill icon filepaths to skills enum, organize resources and remove unnecessary skills

--- src/main/data/Skills.java
+++ src/main/data/Skills.java
@@ -1,5 +1,32 @@
 package data;
 
 public enum Skills {
-	HERBLORE;
+	CONSTRUCTION("Construction", "res/SkillIcons/construction.png"),
+	COOKING("Cooking", "res/SkillIcons/cooking.png"),
+	CRAFTING("Crafting", "res/SkillIcons/crafting.png"),
+	FARMING("Farming", "res/SkillIcons/farming.png"),
+	FIREMAKING("Firemaking", "res/SkillIcons/firemaking.png"),
+	FLETCHING("Fletching", "res/SkillIcons/fletching.png"),
+	HERBLORE("Herblore", "res/SkillIcons/herblore.png"),
+	HUNTER("Hunter", "res/SkillIcons/hunter.png"),
+	MAGIC("Magic", "res/SkillIcons/magic.png"),
+	PRAYER("Prayer", "res/SkillIcons/prayer.png"),
+	SMITHING("Smithing", "res/SkillIcons/smithing.png"),
+	;
+
+	private String name;
+	private String iconPath;
+
+	Skills(String name, String iconPath) {
+		this.name = name;
+		this.iconPath = iconPath;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getIconPath() {
+		return iconPath;
+	}
 }
