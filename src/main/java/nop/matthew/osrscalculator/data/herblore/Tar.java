878ec0f15aac4a659e351ae9a0ee78aade809da8
Matthew C <Matthew.nop.dev@gmail.com>
2025-06-12 17:23:02 -0400

    Herblore: fix incorrect data in `Tar`

--- src/main/java/nop/matthew/osrscalculator/data/herblore/Tar.java
+++ src/main/java/nop/matthew/osrscalculator/data/herblore/Tar.java
@@ -1,6 +1,9 @@
 package nop.matthew.osrscalculator.data.herblore;
 
-import nop.matthew.osrscalculator.data.*;
+import nop.matthew.osrscalculator.data.Flags;
+import nop.matthew.osrscalculator.data.Item;
+import nop.matthew.osrscalculator.data.ItemQuantity;
+import nop.matthew.osrscalculator.data.Recipe;
 
 import java.util.Set;
 
@@ -13,12 +16,20 @@ public enum Tar implements Recipe {
 					new ItemQuantity(Item.GUAM_LEAF, 1),
 					new ItemQuantity(Item.SWAMP_TAR, 15)
 			}),
+	GROUNDGUAM_TAR(
+			new ItemQuantity(Item.GUAM_TAR, 15),
+			19,
+			30,
+			new ItemQuantity[]{
+					new ItemQuantity(Item.GROUND_GUAM, 1),
+					new ItemQuantity(Item.SWAMP_TAR, 15)
+			}),
 	HARRALANDER_TAR(
 			new ItemQuantity(Item.HARRALANDER_TAR, 15),
 			44,
 			72.5f,
 			new ItemQuantity[]{
-					new ItemQuantity(Item.GUAM_LEAF, 1),
+					new ItemQuantity(Item.HARRALANDER, 1),
 					new ItemQuantity(Item.SWAMP_TAR, 15)
 			}),
 	MARRENTILL_TAR(
@@ -26,15 +37,7 @@ public enum Tar implements Recipe {
 			31,
 			42.5f,
 			new ItemQuantity[]{
-					new ItemQuantity(Item.GUAM_LEAF, 1),
-					new ItemQuantity(Item.SWAMP_TAR, 15)
-			}),
-	TARROMIN(
-			new ItemQuantity(Item.TARROMIN, 15),
-			11,
-			5,
-			new ItemQuantity[]{
-					new ItemQuantity(Item.GUAM_LEAF, 1),
+					new ItemQuantity(Item.MARRENTILL, 1),
 					new ItemQuantity(Item.SWAMP_TAR, 15)
 			}),
 	TARROMIN_TAR(
