bd2f23c71c6d13f2fe2b905c5851ca5463ae43c2
Matthew C <Matthew.nop.dev@gmail.com>
2025-12-16 01:42:23 -0500

    Smithing: add platelegs

--- src/main/java/nop/matthew/osrscalculator/data/smithing/Armour.java
+++ src/main/java/nop/matthew/osrscalculator/data/smithing/Armour.java
@@ -9,41 +9,77 @@ import java.net.URL;
 import java.util.Set;
 
 public enum Armour implements Recipe {
+	ADAMANT_PLATEBODY(
+			Item.ADAMANT_PLATEBODY,
+			88,
+			312.5f,
+			new ItemQuantity(Item.ADAMANTITE_BAR, 5)
+	),
+	ADAMANT_PLATELEGS(
+			Item.ADAMANT_PLATELEGS,
+			86,
+			187.5f,
+			new ItemQuantity(Item.ADAMANTITE_BAR, 3)
+	),
 	BRONZE_PLATEBODY(
-		Item.BRONZE_PLATEBODY,
-		18,
-		62.5f,
-		new ItemQuantity(Item.BRONZE_BAR, 5)
+			Item.BRONZE_PLATEBODY,
+			18,
+			62.5f,
+			new ItemQuantity(Item.BRONZE_BAR, 5)
+	),
+	BRONZE_PLATELEGS(
+			Item.BRONZE_PLATELEGS,
+			16,
+			37.5f,
+			new ItemQuantity(Item.BRONZE_BAR, 3)
 	),
 	IRON_PLATEBODY(
-		Item.IRON_PLATEBODY,
-		33,
-		125,
-		new ItemQuantity(Item.IRON_BAR, 5)
+			Item.IRON_PLATEBODY,
+			33,
+			125,
+			new ItemQuantity(Item.IRON_BAR, 5)
 	),
-	STEEL_PLATEBODY(
-		Item.STEEL_PLATEBODY,
-		48,
-		187.5f,
-		new ItemQuantity(Item.STEEL_BAR, 5)
+	IRON_PLATELEGS(
+			Item.IRON_PLATELEGS,
+			31,
+			75,
+			new ItemQuantity(Item.IRON_BAR, 3)
 	),
 	MITHRIL_PLATEBODY(
-		Item.MITHRIL_PLATEBODY,
-		68,
-		250,
-		new ItemQuantity(Item.MITHRIL_BAR, 5)
+			Item.MITHRIL_PLATEBODY,
+			68,
+			250,
+			new ItemQuantity(Item.MITHRIL_BAR, 5)
 	),
-	ADAMANT_PLATEBODY(
-		Item.ADAMANT_PLATEBODY,
-		88,
-		312.5f,
-		new ItemQuantity(Item.ADAMANTITE_BAR, 5)
+	MITHRIL_PLATELEGS(
+			Item.MITHRIL_PLATELEGS,
+			66,
+			150,
+			new ItemQuantity(Item.MITHRIL_BAR, 3)
 	),
 	RUNE_PLATEBODY(
-		Item.RUNE_PLATEBODY,
-		99,
-		375,
-		new ItemQuantity(Item.RUNITE_BAR, 5)
+			Item.RUNE_PLATEBODY,
+			99,
+			375,
+			new ItemQuantity(Item.RUNITE_BAR, 5)
+	),
+	RUNE_PLATELEGS(
+			Item.RUNE_PLATELEGS,
+			99,
+			225,
+			new ItemQuantity(Item.RUNITE_BAR, 3)
+	),
+	STEEL_PLATEBODY(
+			Item.STEEL_PLATEBODY,
+			48,
+			187.5f,
+			new ItemQuantity(Item.STEEL_BAR, 5)
+	),
+	STEEL_PLATELEGS(
+			Item.STEEL_PLATELEGS,
+			46,
+			112.5f,
+			new ItemQuantity(Item.STEEL_BAR, 3)
 	),
 	;
 
